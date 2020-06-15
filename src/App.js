160b1bfae07c5b428591d58b6f69f8a491287a40
Vinh Nguyen <developerthienkim@gmail.com>
2020-06-15 21:24:36 +0700

    add route post and profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Post from './pages/Post';
+import Profile from './pages/Profile';
 
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navigation />
+class App extends Component {
 
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-      </div>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navigation />
+
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/posts" component={Post} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
