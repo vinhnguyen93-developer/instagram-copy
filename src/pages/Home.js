97792fc1b196d5bca4710ebc1e56acc46af51809
Vinh Nguyen <developerthienkim@gmail.com>
2020-06-15 21:19:56 +0700

    add method get data from server

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,58 +1,81 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
+import jwt_decode from 'jwt-decode';
 
 import like from '../assets/heart.png';
 import comment from '../assets/comment.png';
+import { Redirect } from 'react-router-dom';
 
-export default function Home() {
-  return (
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        <div className="col-12 col-sm-6 col-md-6">
-          <div class="card card-container">
-            <div class="card-header">
-              <img src="https://picsum.photos/id/237/200/300" className="rounded-circle" width={38} height={38} />
-              <span className="ml-3">VinhNguyen</span>
-            </div>
-            <img src="https://picsum.photos/id/4/400/500" />
-            <div class="card-body">
-              <img src={like} className="cursor" />
-              <img src={comment} className="ml-2 cursor" />
-              <p className="card-text">9 lượt thích</p>
-              <p class="card-text"><span className="user-name">VinhNguyen</span> Where are you go.</p>
-              <p class="card-text"><span className="user-name">LuanDinh</span> So beautiful</p>
-              <hr/>
-              <form class="form-inline">
-                <div class="form-group mb-2">
-                  <input type="text" className="comment" placeholder="Thêm bình luận..." />
-                </div>
-                <button type="submit" class="btn-active ml-auto">Đăng</button>
-              </form>
-            </div>
-          </div>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
 
-          <div class="card card-container">
-            <div class="card-header">
-              <img src="https://picsum.photos/id/237/200/300" className="rounded-circle" width={38} height={38} />
-              <span className="ml-3">VinhNguyen</span>
+    this.state = {
+      name: '',
+      profilePictureUrl: ''
+    }
+  }
+
+  componentDidMount() {
+    const token = localStorage.usertoken;
+    const decoded = jwt_decode(token);
+
+    this.setState({
+      name: decoded.name,
+      profilePictureUrl: decoded.profilePictureUrl
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-6 col-md-6">
+            <div class="card card-container">
+              <div class="card-header">
+                <img src={this.state.profilePictureUrl} className="rounded-circle" width={38} height={38} />
+                <span className="ml-3">{this.state.name}</span>
+              </div>
+              <img src="https://picsum.photos/id/4/400/500" />
+              <div class="card-body">
+                <img src={like} className="cursor" />
+                <img src={comment} className="ml-2 cursor" />
+                <p className="card-text">9 lượt thích</p>
+                <p class="card-text"><span className="user-name">VinhNguyen</span> Where are you go.</p>
+                <p class="card-text"><span className="user-name">LuanDinh</span> So beautiful</p>
+                <hr/>
+                <form class="form-inline">
+                  <div class="form-group mb-2">
+                    <input type="text" className="comment" placeholder="Thêm bình luận..." />
+                  </div>
+                  <button type="submit" class="btn-active ml-auto">Đăng</button>
+                </form>
+              </div>
             </div>
-            <img src="https://picsum.photos/400/500" />
-            <div class="card-body">
-              <img src={like} className="cursor" />
-              <img src={comment} className="ml-2 cursor" />
-              <p className="card-text">9 lượt thích</p>
-              <p class="card-text"><span className="user-name">VinhNguyen</span> Where are you go.</p>
-              <p class="card-text"><span className="user-name">LuanDinh</span> So beautiful</p>
-              <hr/>
-              <form class="form-inline">
-                <div class="form-group mb-2">
-                  <input type="text" className="comment" placeholder="Thêm bình luận..." />
-                </div>
-                <button type="submit" class="btn-active ml-auto">Đăng</button>
-              </form>
+
+            <div class="card card-container">
+              <div class="card-header">
+                <img src={this.state.profilePictureUrl} className="rounded-circle" width={38} height={38} />
+                <span className="ml-3">{this.state.name}</span>
+              </div>
+              <img src="https://picsum.photos/400/500" />
+              <div class="card-body">
+                <img src={like} className="cursor" />
+                <img src={comment} className="ml-2 cursor" />
+                <p className="card-text">9 lượt thích</p>
+                <p class="card-text"><span className="user-name">VinhNguyen</span> Where are you go.</p>
+                <p class="card-text"><span className="user-name">LuanDinh</span> So beautiful</p>
+                <hr/>
+                <form class="form-inline">
+                  <div class="form-group mb-2">
+                    <input type="text" className="comment" placeholder="Thêm bình luận..." />
+                  </div>
+                  <button type="submit" class="btn-active ml-auto">Đăng</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
